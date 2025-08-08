cd72205b29eceacf51405a05ab0c9e601bd26ac6
Dagbue <dagbuel@gmail.com>
2025-08-08 22:26:43 +0100

    thirtyforth push

--- src/components/DashBoard/DashBoardWithdrawal.vue
+++ src/components/DashBoard/DashBoardWithdrawal.vue
@@ -249,7 +249,6 @@ export default {
 
       if (email === "ravindumunasinghe8@gmail.com"
           || email === "johndoe@yopmail.com"
-          || email === "foggybeatle@gmail.com"
       ) {
         this.showDialog2()
         return;
